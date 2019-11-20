d7c7a164369ce029c47f9174779852277c617b16
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-20 15:49:20 -0500

    add CLIENT_URL and update cors within app to use

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
